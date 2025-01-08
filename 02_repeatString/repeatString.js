db557d6fea33a7f6d9b7f7484fd5fa4c4eb5d1f7
Aarush Gupta <aarushfireblaze@gmail.com>
2025-01-07 20:54:02 -0600

    repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
-
+const repeatString = function(str, num) {
+    let word = str;
+    
+    if (num === 0) {
+        return '';
+    } else if (num <= 0) {
+        return 'ERROR';
+    }
+    for (let i = 1; i < num; i++) {
+        word += str;
+    }
+    return word;
 };
 
 // Do not edit below this line

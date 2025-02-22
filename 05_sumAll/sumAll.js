69cdb75112bd27ecd26362ed7e50a576b389b492
Aarush Gupta <aarushfireblaze@gmail.com>
2025-02-22 14:53:16 -0600

    solved multiple problems

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,27 @@
-const sumAll = function() {
+const sumAll = function(low, high) {
+    if (low < 0 || high < 0){
+        return "ERROR";
+    }
 
+    if (!Number.isInteger(low) || (!Number.isInteger(high))) {
+        return "ERROR";
+    }
+
+    if (isNaN(low) || isNaN(high)) {
+        return "ERROR";
+    }
+
+    if (low > high) {
+        [high, low] = [low, high];
+    }
+
+    let sum = 0;
+
+    for (let i = low; i <= high; i++) {
+        sum += i;
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
